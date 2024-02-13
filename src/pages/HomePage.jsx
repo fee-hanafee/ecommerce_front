9369aff8024f8b74eba9ef7537966b8042620a3e
fee-hanafee <hanafee.pass@gmail.com>
2024-02-13 21:25:08 +0700

    day2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { NAV_BAR_HEIGHT } from "../constans/taiwindConstant";
 
 export default function HomePage() {
+  console.log(NAV_BAR_HEIGHT)
   return (
-    <div className={`bg-red-100 h-[200vh] pt-[${NAV_BAR_HEIGHT}]`}>
+    <div className={`bg-red-100 h-[200vh] pt-[100px]`}>
+      
       <h1>Hello</h1>
     </div>
   );
